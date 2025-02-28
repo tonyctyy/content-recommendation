2f17511f209b7ec6f4577264d53e992fe2f22bbf
SPTonyChan <tonyctyy@gmail.com>
2025-02-28 21:37:02 +0800

    DeepFM update

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from models.ItemCF import ItemCF_predict_user_interests
 from models.DSSM import *
+from models.DeepFM import *
 from retrieve_info import retrieve_business_info, retrieve_user_info
 
 def get_ItemCF_recommendations():
@@ -32,6 +33,30 @@ def get_DSSM_recommendations():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+def get_DeepFM_recommendations():
+    user_id = request.args.get('user_id')
+    
+    # get results from ItemCF
+    ItemCF_k = 300
+    ItemCF_recommendations = ItemCF_predict_user_interests(user_id, ItemCF_k)
+
+    # get results from DSSM
+    DSSM_k = 1000
+    DSSM_recommendations = DSSM_query_top_k(user_id, DSSM_user_model, DSSM_faiss_index, DSSM_business_ids, DSSM_user_id_encoder, DSSM_business_id_encoder, DSSM_user_scaler, DSSM_user_continuous_features_scaled, k=DSSM_k)
+
+    # combine the results
+    business_ids = [business_id for business_id, _ in ItemCF_recommendations + DSSM_recommendations]
+    business_ids = list(set(business_ids))
+    try:
+        user_info = retrieve_user_info(user_id)
+        business_info = retrieve_business_info(business_ids)
+
+        # use DeepFM to rank the businesses
+        recommended_businesses = DeepFM_rank_top_k(DeepFM_model, user_id, business_ids, user_info, business_info, DeepFM_user_id_encoder, DeepFM_business_id_encoder, DeepFM_user_scaler, DeepFM_business_scaler, k=100)
+
+        return jsonify({"user_id": user_id, "recommendations": recommended_businesses, "users": user_info}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 def get_business_info():
     business_ids = request.args.get('business_ids')
